ed4072b2234c7804b2c81a35faf72e6b312d6d0f
Kevin Pedro <kpedro88@gmail.com>
2025-01-02 09:00:06 -0600

    try more options to make training reproducible (pretty close)

--- mdmm_demo_mnist.py
+++ mdmm_demo_mnist.py
@@ -30,18 +30,34 @@ def main():
                    help='load model from this epoch to resume training')
     p.add_argument('--save', type=int, default=1,
                    help='frequency to save models')
+    p.add_argument('--no-mdmm', dest='use_mdmm', default=True, action="store_false",
+                   help='train without mdmm')
+    p.add_argument('--verbose', default=False, action="store_true",
+                   help='enable verbose output')
     args = p.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
-    torch.manual_seed(0)
+    seed = 0
+    torch.manual_seed(seed)
+
+    def worker_init_fn(worker_id):
+        torch.manual_seed(seed)
+        if device.type=='cuda':
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(seed)
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False
+        return
 
     tf = transforms.ToTensor()
+    train_gen = torch.Generator()
+    train_gen.manual_seed(0)
     train_set = datasets.MNIST('data/mnist', download=True, transform=tf)
     train_dl = data.DataLoader(train_set, BATCH_SIZE, shuffle=True,
-                               num_workers=2, pin_memory=True)
+                               num_workers=0, pin_memory=True, generator=train_gen, worker_init_fn=worker_init_fn)
     val_set = datasets.MNIST('data/mnist', train=False, download=True, transform=tf)
-    val_dl = data.DataLoader(val_set, BATCH_SIZE, num_workers=2, pin_memory=True)
+    val_dl = data.DataLoader(val_set, BATCH_SIZE, num_workers=0, pin_memory=True)
 
     model = nn.Sequential(
         nn.Conv2d(1, 16, 3, padding=1),
@@ -53,16 +69,17 @@ def main():
         nn.Flatten(),
         nn.Linear(16 * 7 * 7, 10),
     ).to(device)
-    print('Parameters:', sum(param.numel() for param in model.parameters()))
+    if args.verbose: print('Parameters:', sum(param.numel() for param in model.parameters()))
 
     crit = nn.CrossEntropyLoss()
 
     constraints = []
-    for layer in model:
-        if hasattr(layer, 'weight'):
-            fn = partial(lambda x: x.weight.abs().mean(), layer)
-            constraints.append(mdmm.EqConstraint(fn, args.norm,
-                                                 scale=args.scale, damping=args.damping))
+    if args.use_mdmm:
+        for layer in model:
+            if hasattr(layer, 'weight'):
+                fn = partial(lambda x: x.weight.abs().mean(), layer)
+                constraints.append(mdmm.EqConstraint(fn, args.norm,
+                                                     scale=args.scale, damping=args.damping))
 
     mdmm_module = mdmm.MDMM(constraints)
     opt = mdmm_module.make_optimizer(model.parameters(), lr=args.lr)
@@ -77,6 +94,9 @@ def main():
         opt.load_state_dict(checkpoint['optimizer_state_dict'])
         training_losses = checkpoint['training_losses']
         validation_losses = checkpoint['validation_losses']
+        checkpoint_local = torch.load(loadPath)
+        torch.set_rng_state(checkpoint_local['torch_rng_state'])
+        train_gen.set_state(checkpoint_local['train_gen_state'])
 
     writer = csv.writer(open('mdmm_demo_mnist.csv', 'w'))
     writer.writerow(['loss', 'norm_1', 'norm_2', 'norm_3'])
@@ -97,17 +117,17 @@ def main():
             opt.zero_grad()
             mdmm_return.value.backward()
             opt.step()
-            if i % 100 == 0:
+            if i % 100 == 0 and args.verbose:
                 print(f'{i} {sum(losses[-100:]) / 100:g}')
                 print('Layer weight norms:',
                       *(f'{norm.item():g}' for norm in mdmm_return.fn_values))
         loss = sum(losses) / len(train_dl)
-        print(f'Training loss: {loss.item():g}')
+        if args.verbose: print(f'Training loss: {loss.item():g}')
         return loss.item()
 
     @torch.no_grad()
     def val():
-        print('Validating...')
+        if args.verbose: print('Validating...')
         model.eval()
         losses = []
         for inputs, targets in val_dl:
@@ -117,7 +137,7 @@ def main():
             loss = crit(outputs, targets)
             losses.append(loss * len(outputs))
         loss = sum(losses) / len(val_set)
-        print(f'Validation loss: {loss.item():g}')
+        if args.verbose: print(f'Validation loss: {loss.item():g}')
         return loss.item()
 
     def save(epoch):
@@ -126,12 +146,15 @@ def main():
             'optimizer_state_dict': opt.state_dict(),
             'training_losses': training_losses,
             'validation_losses': validation_losses,
+            'torch_rng_state': torch.get_rng_state(),
+            'train_gen_state': train_gen.get_state(),
         }
         torch.save(checkpoint, f'model_{epoch}.pt')
 
     try:
-        initial_epoch = 0
-        if args.load: initial_epoch = args.load + 1
+        initial_epoch = 1
+        if args.load:
+            initial_epoch = args.load + 1
         final_epoch = initial_epoch + args.epochs
         for epoch in range(initial_epoch, final_epoch):
             print('Epoch', epoch)
