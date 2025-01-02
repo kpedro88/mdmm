e3b22a60bbb219f2e66bb29116cae1eff5af8582
Kevin Pedro <kpedro88@gmail.com>
2025-01-02 05:30:59 -0600

    add options to save and load models

--- mdmm_demo_mnist.py
+++ mdmm_demo_mnist.py
@@ -13,8 +13,6 @@ from torch.utils import data
 from torchvision import datasets, transforms
 
 BATCH_SIZE = 50
-EPOCHS = 100
-
 
 def main():
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -26,6 +24,12 @@ def main():
                    help='the damping strength')
     p.add_argument('--lr', type=float, default=0.02,
                    help='the learning rate')
+    p.add_argument('--epochs', type=int, default=100,
+                   help='train for this many epochs')
+    p.add_argument('--load', type=int, default=None,
+                   help='load model from this epoch to resume training')
+    p.add_argument('--save', type=int, default=1,
+                   help='frequency to save models')
     args = p.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -63,6 +67,17 @@ def main():
     mdmm_module = mdmm.MDMM(constraints)
     opt = mdmm_module.make_optimizer(model.parameters(), lr=args.lr)
 
+    training_losses = []
+    validation_losses = []
+
+    if args.load:
+        loadPath = f'model_{args.load}.pt'
+        checkpoint = torch.load(loadPath, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        opt.load_state_dict(checkpoint['optimizer_state_dict'])
+        training_losses = checkpoint['training_losses']
+        validation_losses = checkpoint['validation_losses']
+
     writer = csv.writer(open('mdmm_demo_mnist.csv', 'w'))
     writer.writerow(['loss', 'norm_1', 'norm_2', 'norm_3'])
 
@@ -86,6 +101,9 @@ def main():
                 print(f'{i} {sum(losses[-100:]) / 100:g}')
                 print('Layer weight norms:',
                       *(f'{norm.item():g}' for norm in mdmm_return.fn_values))
+        loss = sum(losses) / len(train_dl)
+        print(f'Training loss: {loss.item():g}')
+        return loss.item()
 
     @torch.no_grad()
     def val():
@@ -100,12 +118,28 @@ def main():
             losses.append(loss * len(outputs))
         loss = sum(losses) / len(val_set)
         print(f'Validation loss: {loss.item():g}')
+        return loss.item()
+
+    def save(epoch):
+        checkpoint = {
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': opt.state_dict(),
+            'training_losses': training_losses,
+            'validation_losses': validation_losses,
+        }
+        torch.save(checkpoint, f'model_{epoch}.pt')
 
     try:
-        for epoch in range(1, EPOCHS + 1):
+        initial_epoch = 0
+        if args.load: initial_epoch = args.load + 1
+        final_epoch = initial_epoch + args.epochs
+        for epoch in range(initial_epoch, final_epoch):
             print('Epoch', epoch)
-            train()
-            val()
+            _ = train()
+            training_losses.append(_)
+            _ = val()
+            validation_losses.append(_)
+            if epoch % args.save == 0 or epoch == final_epoch: save(epoch)
     except KeyboardInterrupt:
         pass
 
